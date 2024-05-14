13e9e129c048210e08e904f2c5ef8498fde95395
Salter11 <salte010@csusm.edu>
2024-05-14 15:29:22 -0700

    Worked on LoadLexicon, GetEWord and Gen functions

--- Part_C_Translator/translator.cpp
+++ Part_C_Translator/translator.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<map>
+#include<sstream>
 using namespace std;
 
-/* INSTRUCTION:  copy your parser.cpp here
-      cp ../ParserFiles/parser.cpp .
+/* INSTRUCTION: copy your parser.cpp here
+   cp ../ParserFiles/parser.cpp .
    Then, insert or append its contents into this file and edit.
    Complete all ** parts.
 */
@@ -13,56 +15,102 @@ using namespace std;
 // File translator.cpp written by Group Number: **
 //=================================================
 
-// ----- Additions to the parser.cpp ---------------------
+map<string, string> Lexicon;  // Global variable for the lexicon
+string saved_lexeme, saved_E_word, saved_token;  // Global variables for word management
+ofstream outFile;  // File stream for output
 
+// Function to load lexicon from a file
+void loadLexicon() {
+    ifstream lexFile("lexicon.txt");
+    if (!lexFile.is_open()) {
+        cerr << "Failed to open lexicon.txt" << endl;
+        return;
+    }
+
+    string line;
+    while (getline(lexFile, line)) {
+        istringstream iss(line);
+        string japWord, engWord;
+
+        // Read the Japanese word (first word on each line)
+        iss >> japWord;
+
+        // Read the rest of the line as the English translation
+        getline(iss, engWord);
+
+        // Trim leading spaces from the English word
+        size_t start = engWord.find_first_not_of(" \t");
+        engWord = (start == string::npos) ? "" : engWord.substr(start);
+
+        Lexicon[japWord] = engWord;
+    }
+    lexFile.close();
+}
 // ** Declare Lexicon (i.e. dictionary) that will hold the content of lexicon.txt
 // Make sure it is easy and fast to look up the translation.
-// Do not change the format or content of lexicon.txt 
-//  Done by: ** 
+// Do not change the format or content of lexicon.txt
+//  Done by: **Adam Salter** 
 
+// Function to translate Japanese words to English
+void getEword() {
+    auto it = Lexicon.find(saved_lexeme);
+    if (it != Lexicon.end()) {
+        saved_E_word = it->second;
+    } else {
+        saved_E_word = saved_lexeme; // If not found, use the Japanese word as a fallback
+    }
+}
+//  Done by: **Adam Salter** 
 
-// ** Additions to parser.cpp here:
-//    getEword() - using the current saved_lexeme, look up the English word
-//                 in Lexicon if it is there -- save the result   
-//                 in saved_E_word
-//  Done by: ** 
-//    gen(line_type) - using the line type,
-//                     sends a line of an IR to translated.txt
-//                     (saved_E_word or saved_token is used)
-//  Done by: ** 
-
-// ----- Changes to the parser.cpp content ---------------------
-
-// ** Comment update: Be sure to put the corresponding grammar 
-//    rule with semantic routine calls
-//    above each non-terminal function 
-
-// ** Each non-terminal function should be calling
-//    getEword and/or gen now.
-
+// Function to generate lines of IR and write to translated.txt
+void gen(const string& line_type) {
+    if (!outFile.is_open()) {
+        cerr << "Output file not open for writing." << endl;
+        return;
+    }
+    outFile << line_type << ": ";
+    if (line_type == "TENSE") {
+        outFile << saved_token; // TENSE uses tokens
+    } else {
+        outFile << saved_E_word; // Other types use the saved English word
+    }
+    outFile << endl;
+}
+//  Done by: *Adam Salter* 
 
 // ---------------- Driver ---------------------------
 
 // The final test driver to start the translator
-// Done by:  **
 int main()
 {
-  //** opens the lexicon.txt file and reads it into Lexicon
-  //** closes lexicon.txt 
+    string filename;
+    ifstream fin;
 
-  //** opens the output file translated.txt
+    loadLexicon(); // Load the lexicon for translations
+    outFile.open("translated.txt"); // Open the output file for IR generation
+    if (!outFile.is_open()) {
+        cerr << "Failed to open translated.txt" << endl;
+        return -1;
+    }
 
-  cout << "Enter the input file name: ";
-  cin >> filename;
-  fin.open(filename.c_str());
+    cout << "Enter the input file name: ";
+    cin >> filename;
+    fin.open(filename.c_str());
+    if (!fin.is_open()) {
+        cerr << "Failed to open input file." << endl;
+        outFile.close(); // Close outFile before exiting
+        return -1;
+    }
 
-  //** calls the <story> to start parsing
+    // Placeholder for the call to the <story> function
+    // story(); // This function should be defined based on your grammar
 
-  //** closes the input file 
-  //** closes traslated.txt
- 
-}// end
-//** require no other input files!
-//** syntax error EC requires producing errors.txt of error messages
-//** tracing On/Off EC requires sending a flag to trace message output functions
+    fin.close(); // Close the input file
+    outFile.close(); // Close the output file
 
+    return 0;
+}
+// Done by: **
+// ** require no other input files!
+// ** syntax error EC requires producing errors.txt of error messages
+// ** tracing On/Off EC requires sending a flag to trace message output functions
