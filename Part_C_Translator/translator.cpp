80c5e9a0aad8db3cfa293157d5bdac8b2f5dcd90
Hando19 <agustin.alex132@gmail.com>
2024-05-15 09:59:52 -0700

    Story function
    Filled in for story function based off the lefthand rules we did

--- Part_C_Translator/translator.cpp
+++ Part_C_Translator/translator.cpp
@@ -12,12 +12,13 @@ using namespace std;
 */
 
 //=================================================
-// File translator.cpp written by Group Number: **
+// File translator.cpp written by Group Number: 10
 //=================================================
 
 map<string, string> Lexicon;  // Global variable for the lexicon
 string saved_lexeme, saved_E_word, saved_token;  // Global variables for word management
 ofstream outFile;  // File stream for output
+ifstream fin;  // File stream for input
 
 // Function to load lexicon from a file
 void loadLexicon() {
@@ -49,7 +50,7 @@ void loadLexicon() {
 // ** Declare Lexicon (i.e. dictionary) that will hold the content of lexicon.txt
 // Make sure it is easy and fast to look up the translation.
 // Do not change the format or content of lexicon.txt
-//  Done by: **Adam Salter** 
+// Done by: **Adam Salter** 
 
 // Function to translate Japanese words to English
 void getEword() {
@@ -60,7 +61,7 @@ void getEword() {
         saved_E_word = saved_lexeme; // If not found, use the Japanese word as a fallback
     }
 }
-//  Done by: **Adam Salter** 
+// Done by: **Adam Salter** 
 
 // Function to generate lines of IR and write to translated.txt
 void gen(const string& line_type) {
@@ -76,15 +77,105 @@ void gen(const string& line_type) {
     }
     outFile << endl;
 }
-//  Done by: *Adam Salter* 
+// Done by: **Adam Salter and Alejandro Agustin** 
 
-// ---------------- Driver ---------------------------
+// Function to get the next token from the input file
+void getNextToken() {
+    fin >> saved_token >> saved_lexeme;
+}
 
-// The final test driver to start the translator
-int main()
-{
+void afterObject();
+void afterNoun();
+void afterSubject();
+void s();
+void story();
+
+// Function to handle the <story> rule
+void story() {
+    s();
+    while (fin >> saved_token) {
+        fin.putback(saved_token[0]);
+        s();
+    }
+}
+
+// Function to handle the <s> rule
+void s() {
+    getNextToken();
+    if (saved_token == "CONNECTOR") {
+        getEword();
+        gen("CONNECTOR");
+        getNextToken();
+    }
+    // Assuming <noun> is processed here
+    getEword();
+    gen("ACTOR");
+    afterSubject();
+}
+
+// Function to handle the <after subject> rule
+void afterSubject() {
+    getNextToken();
+    if (saved_token == "VERB") {
+        getEword();
+        gen("ACTION");
+        getNextToken(); // Get tense
+        gen("TENSE");
+        getNextToken(); // Get period
+    } else {
+        // Assuming <noun> is processed here
+        getEword();
+        afterNoun();
+    }
+}
+
+// Function to handle the <after noun> rule
+void afterNoun() {
+    getNextToken();
+    if (saved_token == "BE") {
+        gen("DESCRIPTION");
+        getNextToken(); // Get tense
+        gen("TENSE");
+        getNextToken(); // Get period
+    } else if (saved_token == "DESTINATION") {
+        gen("TO");
+        getNextToken(); // Get verb
+        getEword();
+        gen("ACTION");
+        getNextToken(); // Get tense
+        gen("TENSE");
+        getNextToken(); // Get period
+    } else if (saved_token == "OBJECT") {
+        gen("OBJECT");
+        afterObject();
+    }
+}
+
+// Function to handle the <after object> rule
+void afterObject() {
+    getNextToken();
+    if (saved_token == "VERB") {
+        getEword();
+        gen("ACTION");
+        getNextToken(); // Get tense
+        gen("TENSE");
+        getNextToken(); // Get period
+    } else if (saved_token == "NOUN") {
+        getEword();
+        getNextToken(); // Get destination
+        gen("TO");
+        getNextToken(); // Get verb
+        getEword();
+        gen("ACTION");
+        getNextToken(); // Get tense
+        gen("TENSE");
+        getNextToken(); // Get period
+    }
+}
+
+// Driver function
+int main() {
     string filename;
-    ifstream fin;
 
     loadLexicon(); // Load the lexicon for translations
     outFile.open("translated.txt"); // Open the output file for IR generation
@@ -102,15 +193,14 @@ int main()
         return -1;
     }
 
-    // Placeholder for the call to the <story> function
-    // story(); // This function should be defined based on your grammar
+    story(); // Call the story function to process the input file
 
     fin.close(); // Close the input file
     outFile.close(); // Close the output file
 
     return 0;
 }
-// Done by: **
-// ** require no other input files!
-// ** syntax error EC requires producing errors.txt of error messages
-// ** tracing On/Off EC requires sending a flag to trace message output functions
+// Done by: **Adam Salter**
+// require no other input files!
+// syntax error EC requires producing errors.txt of error messages
+// tracing On/Off EC requires sending a flag to trace message output functions
